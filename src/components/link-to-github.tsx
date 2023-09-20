76b4def8e3abeb547afeff5421aa7e42bf9481bc
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-20 12:04:25 -0300

    building-link-to-github-component

---
+++ src/components/link-to-github.tsx
@@ -0,0 +1,27 @@
+import { Github } from 'lucide-react'
+import { HoverCard, HoverCardTrigger, HoverCardContent } from './ui/hover-card'
+
+export const GithubLink = () => {
+  return (
+    <HoverCard>
+      <HoverCardTrigger>
+        <Github className='h-5 w-5 cursor-pointer' />
+      </HoverCardTrigger>
+      <HoverCardContent className='mr-2'>
+        <div className='flex gap-4'>
+          <img
+            src="https://github.com/MatheusMottaGit.png"
+            className='w-12 h-12 rounded-full'
+          />
+
+          <div className='flex flex-col flex-1 gap-1'>
+            <h2 className='text-base font-bold'>Matheus Domingues Motta</h2>
+
+            <a href="https://github.com/MatheusMottaGit" className='text-sm underline text-neutral-500'>Acessar Github</a>
+          </div>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+
+  )
+}
