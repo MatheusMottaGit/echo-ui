e1d020d5ed6098348e9129d6629674df6f4f8c5a
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-22 12:32:30 -0300

    building-root-layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Blocks, Gavel } from "lucide-react";
-import { ModeToggle } from "./components/toggle-theme";
-import { GithubLink } from "./components/link-to-github";
-import { Button } from "./components/ui/button";
 import PortfolioCard from "./components/cards/portfolio-card";
 import AboutMeCard from "./components/cards/about-me";
 import ContactMeCard from "./components/cards/contact-me";
+import PagesLayout from "./components/pages-layout";
 
 const words = [
   'Interatividade.',
@@ -14,47 +11,27 @@ const words = [
 
 export function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="flex items-center justify-between border-b px-4 py-2">
-        <div className="flex items-center gap-2">
-          <Blocks className="h-5 w-5" />
-          <h1 className="font-bold font-sans text-lg">echo\ui</h1>
-        </div>
+    <PagesLayout>
+      <div className="flex flex-col items-center gap-4 font-sans font-bold">
+        <h1 className="text-5xl">Esta é uma página para</h1>
 
-        <div className="flex items-center gap-2">
-          <Button variant='outline' className="gap-2 mr-3">
-            Tools
-            <Gavel className="w-4 h-4" />
-          </Button>
-
-          <GithubLink />
-
-          <ModeToggle />
-        </div>
+        <span className="text-4xl text-zinc-600 blinking-cursor">
+          {/* Typing effect words */}
+        </span>
       </div>
 
-      <main className="px-4 py-6 flex flex-col gap-10 flex-1 items-center">
-        <div className="flex flex-col items-center gap-4 font-sans font-bold">
-          <h1 className="text-5xl">Esta é uma página para</h1>
-
-          <span className="text-4xl text-zinc-600 blinking-cursor">
-            {/* Typing effect words */}
-          </span>
+      <section className="flex flex-col flex-1 border rounded-lg p-4 w-full">
+        <div className="grid grid-rows-2 gap-2 text-center">
+          <h1 className="text-3xl font-sans font-bold">Navegue entre as opções</h1>
+          <span className="text-zinc-600">Espero que goste! 🤍</span>
         </div>
 
-        <section className="flex flex-col flex-1 border rounded-lg p-4 w-full">
-          <div className="grid grid-rows-2 gap-2 text-center">
-            <h1 className="text-3xl font-sans font-bold">Navegue entre as opções</h1>
-            <span className="text-zinc-600">Espero que goste! 🤍</span>
-          </div>
-
-          <div className="grid grid-cols-3 gap-3 place-items-center justify-items-center">
-            <PortfolioCard />
-            <AboutMeCard />
-            <ContactMeCard />
-          </div>
-        </section>
-      </main>
-    </div>
+        <div className="grid grid-cols-3 gap-3 place-items-center justify-items-center">
+          <PortfolioCard />
+          <AboutMeCard />
+          <ContactMeCard />
+        </div>
+      </section>
+    </PagesLayout>
   )
 }
---
+++ src/components/pages-layout.tsx
@@ -0,0 +1,35 @@
+import { Blocks, Gavel } from 'lucide-react'
+import React, { ReactNode } from 'react'
+import { GithubLink } from './link-to-github'
+import { ModeToggle } from './toggle-theme'
+import { Button } from './ui/button'
+
+const PagesLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="flex items-center justify-between border-b px-4 py-2">
+        <div className="flex items-center gap-2">
+          <Blocks className="h-5 w-5" />
+          <h1 className="font-bold font-sans text-lg">echo\ui</h1>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Button variant='outline' className="gap-2 mr-3">
+            Tools
+            <Gavel className="w-4 h-4" />
+          </Button>
+
+          <GithubLink />
+
+          <ModeToggle />
+        </div>
+      </div>
+
+      <main className="px-4 py-6 flex flex-col gap-10 flex-1">
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default PagesLayout
