6ebc88adfaef69f111a60e3decda5378b5cb3b17
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-26 20:08:17 -0300

    building-tabs

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,8 +1,8 @@
 import PagesLayout from '@/components/layouts/pages-layout'
-import TabsLayout from '@/components/layouts/tabs-layout'
 import RepositoriesList from '@/components/repositories-list'
 import { Card } from '@/components/ui/card'
-import { TabsContent } from '@/components/ui/tabs'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Tag, FileCode2 } from 'lucide-react'
 import { useState, useEffect } from 'react'
 
 export interface Repo {
@@ -32,17 +32,27 @@ const Projects = () => {
         <h1 className='font-sans font-bold text-4xl -mt-1'>Meus projetos</h1>
       </div>
 
-      <section className='flex-1'>
-        <TabsLayout>
-          <TabsContent value='principais' className='grid grid-cols-2 gap-3'>
-            <Card className='p-3 bg-iron bg-cover opacity-50 hover:opacity-70 transition-all'></Card>
-            <Card className='p-3 bg-pype bg-cover row-span-2 opacity-50 hover:opacity-70 transition-all'></Card>
-            <Card className='p-3 bg-restaurant bg-cover opacity-50 hover:opacity-70 transition-all'></Card>
-          </TabsContent>
+      <Tabs className='flex-1' defaultValue='principais'>
+        <TabsList className='flex flex-1 justify-around'>
+          <TabsTrigger className='w-full gap-2' value='principais'>
+            Principais
+            <Tag className='w-4 h-4' />
+          </TabsTrigger>
+          <TabsTrigger className='w-full gap-2' value='todos'>
+            Todos
+            <FileCode2 className='w-4 h-4' />
+          </TabsTrigger>
+        </TabsList>
 
-          <RepositoriesList repos={repos} />
-        </TabsLayout>
-      </section>
+        <TabsContent value='principais' className='grid grid-cols-2 gap-3'>
+          <Card className='p-3 bg-iron bg-cover opacity-50 hover:opacity-70 transition-all'></Card>
+          <Card className='p-3 bg-pype bg-cover row-span-2 opacity-50 hover:opacity-70 transition-all'></Card>
+          <Card className='p-3 bg-restaurant bg-cover opacity-50 hover:opacity-70 transition-all'></Card>
+        </TabsContent>
+
+        <RepositoriesList repos={repos} />
+
+      </Tabs>
     </PagesLayout>
   )
 }
