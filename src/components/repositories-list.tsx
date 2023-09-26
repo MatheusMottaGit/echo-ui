010b96e5d3f1a4b0bb85f5ced102e2b730634dc0
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-26 15:52:27 -0300

    correcting-repos-list

--- src/components/repositories-list.tsx
+++ src/components/repositories-list.tsx
@@ -1,72 +1,61 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card'
+import { Card, CardContent, CardDescription, CardTitle } from './ui/card'
 import { useGithub } from '@/hooks/useGithub'
 import { TabsContent } from './ui/tabs'
-import TabsLayout from './layouts/tabs-layout'
 import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card'
 import { Square } from 'lucide-react'
 import { getDate } from '@/lib/date'
+import { Repo } from '@/pages/projects'
 
-interface SearchedRepoProps {
-  search: string
+interface GetRepoProps {
+  repos: Repo[]
 }
 
-const RepositoriesList = ({ }: SearchedRepoProps) => {
-  const { repos } = useGithub()
-
-  // const filteredRepos = search.length > 0 ? repos.filter(repo => repo.name.includes(search)) : []
-
+const RepositoriesList = ({ repos }: GetRepoProps) => {
   return (
     <>
-      <TabsLayout>
-        <TabsContent value='principais' className='grid grid-cols-2 gap-3'>
-          <Card className='p-3 bg-iron bg-cover h-52 opacity-50 hover:opacity-70 transition-all'></Card>
-          <Card className='p-3 bg-pype bg-cover row-span-2 opacity-50 hover:opacity-70 transition-all'></Card>
-          <Card className='p-3 bg-restaurant bg-cover h-52 opacity-50 hover:opacity-70 transition-all'></Card>
-        </TabsContent>
-
-        <TabsContent value='todos' className='grid grid-cols-1 gap-3'>
-          <div className='flex items-center justify-between gap-4 text-zinc-400'>
-            <div className='flex gap-5'>
-              <div className='flex items-center gap-1'>
-                <Square className='h-4 w-4 bg-green-700 text-green-700 rounded-sm' />
-                Repositório mais recente
-              </div>
-
-              <div className='flex items-center gap-1'>
-                <Square className='h-4 w-4 text-green-500 bg-green-500 rounded-sm' />
-                Repositório menos recente
-              </div>
+      <TabsContent value='todos' className='flex flex-col gap-5'>
+        <div className='flex items-center justify-between gap-4 text-zinc-400'>
+          <div className='flex gap-5'>
+            <div className='flex items-center gap-1'>
+              <Square className='h-4 w-4 bg-green-700 text-green-700 rounded-sm' />
+              Repositório mais recente
             </div>
 
-            <span className='italic text-zinc-700 text-sm'>
-              * Passe o mouse por cima dos cards
-            </span>
+            <div className='flex items-center gap-1'>
+              <Square className='h-4 w-4 text-green-500 bg-green-500 rounded-sm' />
+              Repositório menos recente
+            </div>
           </div>
 
-          <div className='grid grid-cols-12 gap-y-3'>
-            {repos.map(repo => {
-              const repoCreationDate = getDate(repo.created_at)
+          <span className='italic text-zinc-700 text-sm'>
+            * Passe o mouse por cima dos cards
+          </span>
+        </div>
 
-              return (
-                <HoverCard key={repo.id}>
-                  <HoverCardTrigger>
-                    <Card className={`w-24 h-24 cursor-pointer`}></Card>
-                  </HoverCardTrigger>
-                  <HoverCardContent>
-                    <CardTitle>{repo.name}</CardTitle>
-                    <CardDescription>Data de criação: {repoCreationDate}</CardDescription>
+        <div className='grid grid-cols-12 gap-y-3'>
+          {repos.map(repo => {
+            const repoCreationDate = getDate(repo.created_at)
 
-                    <CardContent className='p-0 mt-4 text-zinc-500'>{repo.description}</CardContent>
-                  </HoverCardContent>
-                </HoverCard>
-              )
-            })}
-          </div>
-        </TabsContent>
-      </TabsLayout>
+            return (
+              <HoverCard key={repo.id}>
+                <HoverCardTrigger>
+                  <Card className={`w-24 h-24 cursor-pointer`}></Card>
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <CardTitle>{repo.name}</CardTitle>
+                  <CardDescription>Data de criação: {repoCreationDate}</CardDescription>
+
+                  <CardContent className='p-0 mt-4 text-zinc-500'>{repo.description}</CardContent>
+                </HoverCardContent>
+              </HoverCard>
+            )
+          })}
+        </div>
+      </TabsContent>
     </>
   )
 }
 
 export default RepositoriesList
+
 
