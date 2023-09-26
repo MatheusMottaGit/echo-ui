7e3dd74c2b74efcb9aa743c49ef767aed3d5b462
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-26 15:52:15 -0300

    building-courses-card

--- src/components/cards/courses-card.tsx
+++ src/components/cards/courses-card.tsx
@@ -8,21 +8,21 @@ const CoursesCard = () => {
 
       <div className='mt-4 grid grid-cols-2 gap-3 flex-1'>
         <Card className='p-2 flex items-center gap-2 cursor-pointer'>
-          <Award className='h-8 w-8' />
+          <Award className='h-6 w-6' />
           <div className='flex flex-col'>
             <h2>FAETEC - Volta Redonda</h2>
             <span className='text-xs text-muted-foreground'>Técnico - Informática para Internet</span>
           </div>
         </Card>
         <Card className='p-2 flex items-center gap-2 cursor-pointer'>
-          <GraduationCap className='h-8 w-8' />
+          <GraduationCap className='h-6 w-6' />
           <div className='flex flex-col'>
             <h2>Unifoa - Volta Redonda</h2>
             <span className='text-xs text-muted-foreground'>Bacharelado - Sistemas de Informação</span>
           </div>
         </Card>
         <Card className='p-2 flex items-center gap-2 cursor-pointer col-span-2'>
-          <Languages className='h-8 w-8' />
+          <Languages className='h-6 w-6' />
           <div className='flex flex-col'>
             <h2>Yázigi</h2>
             <span className='text-xs text-muted-foreground'>Inglês - Básico, intermediário e avançado</span>
