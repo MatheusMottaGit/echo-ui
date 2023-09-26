70c424a391f8bd61959b6d4d36be8ec79b85063c
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-26 15:52:38 -0300

    deleting-context

--- src/hooks/useGithub.ts
+++
@@ -1,7 +0,0 @@
-import { GithubContext } from "@/contexts/GithubContext"
-import { useContext } from "react"
-
-export const useGithub = () => {
-  const context = useContext(GithubContext)
-  return context
-}
