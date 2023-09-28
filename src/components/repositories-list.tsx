9dadae02bae3fcaf0d966605c7e5b00c37210b82
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-28 09:49:27 -0300

    testing-dates

--- src/components/repositories-list.tsx
+++ src/components/repositories-list.tsx
@@ -12,7 +12,7 @@ interface ListRepoProps {
 }
 
 const RepositoriesList = ({ repos }: ListRepoProps) => {
-  const today = dayjs()
+  // const today = dayjs()
 
   return (
     <>
@@ -35,27 +35,27 @@ const RepositoriesList = ({ repos }: ListRepoProps) => {
           </span>
         </div>
 
-        <div className='grid grid-cols-12 gap-y-3'>
+        <div className='grid grid-cols-12 gap-y-3 max-[1269px]:grid-cols-11 max-[1143px]:grid-cols-10 max-[1080px]:grid-cols-9 max-[940px]:grid-cols-8 max-[843px]:grid-cols-7 max-[769px]:grid-cols-6 max-[781px]:place-items-center max-[642px]:grid-cols-5 max-[546px]:grid-cols-4 max-[416px]:grid-cols-3 max-[320px]:gap-x-5'>
           {repos.map(repo => {
             const repoCreationDate = getDate(repo.created_at)
 
-            const differenceBetweenDatesMonths = today.diff(repo.created_at, 'month')
+            // const differenceBetweenDatesMonths = today.diff(repo.created_at, 'month')
 
             return (
               <HoverCard key={repo.id}>
                 <HoverCardTrigger>
                   <Card className={clsx('w-24 h-24 cursor-pointer', {
-                    'bg-green-400': differenceBetweenDatesMonths === 0,
-                    'bg-green-500': differenceBetweenDatesMonths === 1,
-                    'bg-green-600': differenceBetweenDatesMonths === 2,
-                    'bg-green-700': differenceBetweenDatesMonths === 3,
-                    'bg-green-800': differenceBetweenDatesMonths === 4,
-                    'bg-green-900': differenceBetweenDatesMonths === 5,
-                    'bg-green-900/50': differenceBetweenDatesMonths === 6,
-                    'bg-green-400/25': differenceBetweenDatesMonths === 7,
-                    'bg-green-400/20': differenceBetweenDatesMonths === 8,
-                    'bg-green-400/10': differenceBetweenDatesMonths === 9,
-                    'bg-green-400/5': differenceBetweenDatesMonths >= 10,
+                    // 'bg-green-400': differenceBetweenDatesMonths === 0,
+                    // 'bg-green-500': differenceBetweenDatesMonths === 1,
+                    // 'bg-green-600': differenceBetweenDatesMonths === 2,
+                    // 'bg-green-700': differenceBetweenDatesMonths === 3,
+                    // 'bg-green-800': differenceBetweenDatesMonths === 4,
+                    // 'bg-green-900': differenceBetweenDatesMonths === 5,
+                    // 'bg-green-900/50': differenceBetweenDatesMonths === 6,
+                    // 'bg-green-400/25': differenceBetweenDatesMonths === 7,
+                    // 'bg-green-400/20': differenceBetweenDatesMonths === 8,
+                    // 'bg-green-400/10': differenceBetweenDatesMonths === 9,
+                    // 'bg-green-400/5': differenceBetweenDatesMonths >= 10,
                   })}>
                   </Card>
                 </HoverCardTrigger>
